37353f890987651da4726f54a863826ca7bee05d
星野拓朗 <hosino187@gmail.com>
2019-01-18 15:30:19 +0900

    no message

--- config/database.rb
+++ config/database.rb
@@ -19,10 +19,10 @@ ActiveRecord::Base.configurations[:development] = {
   reconnect: true,
   database: 'contact_app',
   pool: 5,
-  username: 'root',
-  password: '123456',
-  host: '127.0.0.1',
-  port: '3308'
+  username: '**********',
+  password: '**********',
+  host: '**********',
+  port: '**********'
 
 }
 
